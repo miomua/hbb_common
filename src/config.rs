0b7d70e229c8cd129f7ed00ad311743f6b403198
miomua <33140872+miomua@users.noreply.github.com>
2025-03-29 12:32:43 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -102,8 +102,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["ssx.f3322.net"];
+pub const RS_PUB_KEY: &str = "naG3YkwBMupqM6dMSjIgN46XvbNFYfbuY3xXKTXRAJ4";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
